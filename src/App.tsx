4d7498a827eabd46e2e5432010280ecec2fe9d58
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-02-02 17:58:49 -0300

    feature: add section member and footer

--- src/App.tsx
+++ src/App.tsx
@@ -313,6 +313,117 @@ export default function App() {
 </div>
 
 </section>
+ {/*section review*/}
+ <section className="review">
+
+<div className="section__container review__container">
+
+  <span><i className="ri-chat-quote-line"></i></span>
+
+  <div className="review__content">
+
+    <h4>MEMBER REVIEW</h4>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Culpa, nihil, totam ea sit doloribus libero, quos earum
+      iure consequatur in quas enim ad a vitae quasi nemo rerum expedita non?
+    </p>
+
+    <div className="review__rating">
+      <span><i className="ri-star-fill"></i></span>
+      <span><i className="ri-star-fill"></i></span>
+      <span><i className="ri-star-fill"></i></span>
+      <span><i className="ri-star-fill"></i></span>
+      <span><i className="ri-star-fill"></i></span>
+    </div>
+
+    <div className="review__footer">
+
+      <div className="review__member">
+
+        <img src={member} alt="member" />
+
+        <div className="review__member__details">
+          <h4>Jane Cooper</h4>
+          <p>Software developer</p>
+        </div>
+
+      </div>
+
+      <div className="review__nav">
+        <span><i className="ri-arrow-left-line"></i></span>
+        <span><i className="ri-arrow-right-line"></i></span>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+</section>
+
+{/*footer*/}
+<footer className="section__container footer__container">
+
+<span className="bg__blur"></span>
+<span className="bg__blur footer__blur"></span>
+
+{/*logo*/}
+<div className="footer__col">
+
+  <div className="footer__logo">
+    <img src={logo} alt="logo" />
+  </div>
+
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Unde corrupti molestiae, voluptatem non ipsam impedit
+    numquam neque nisi fugiat
+  </p>
+
+  {/*logo social media*/}
+  <div className="footer__socials">
+    <a href="#"><i className="ri-facebook-circle-fill"></i></a>
+    <a href="#"><i className="ri-instagram-fill"></i></a>
+    <a href="#"><i className="ri-twitter-x-fill"></i></a>
+  </div>
+
+</div>
+
+{/*column footer*/}
+<div className="footer__col">
+  <h4>Company</h4>
+  <a href="#">Bussiness</a>
+  <a href="#">Franchise</a>
+  <a href="#">Partnership</a>
+  <a href="#">Network</a>
+</div>
+
+{/*column footer*/}
+<div className="footer__col">
+  <h4>About Us</h4>
+  <a href="#">Blogs</a>
+  <a href="#">Security</a>
+  <a href="#">Careers</a>
+</div>
+
+{/*column footer*/}
+<div className="footer__col">
+  <h4>Contact</h4>
+  <a href="#">Contact us</a>
+  <a href="#">Privacy Policy</a>
+  <a href="#">Terms & Conditions</a>
+  <a href="#">BMI Calculator</a>
+</div>
+
+</footer>
+
+<div className="footer__bar">
+© 2026 Academia. Todos os direitos reservados.
+</div>
+
 
     </>
   );
