ec3ae509ba975a69964b271fccdcc818f8d78263
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-02-02 17:37:21 -0300

    feature: add nav menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import "./styles/styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import class1 from "./assets/class-1.jpg";
+import class2 from "./assets/class-2.jpg";
+import header from "./assets/header.png";
+import join from "./assets/join.jpg";
+import logo from "./assets/logo.png";
+import member from "./assets/member.jpg";
 
+
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {/*menu*/}
+      <nav>
+        <div className="nav__logo">
+          <a href="#"><img src={logo} alt="logo" /></a>
+        </div>
+        <ul className="nav__links">
+          <li className="link"><a href="#">Home</a></li>
+          <li className="link"><a href="#">Program</a></li>
+          <li className="link"><a href="#">Service</a></li>
+          <li className="link"><a href="#">About</a></li>
+          <li className="link"><a href="#">Community</a></li>
+        </ul>
+        <button className="btn">Join Now</button>
+      </nav>
+          </>
+  );
 }
-
-export default App
