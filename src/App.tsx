0ff3580a8c46985138b841ab0d585f442d635f67
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-02-02 17:42:01 -0300

    feature: add hero

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,34 @@ export default function App() {
         </ul>
         <button className="btn">Join Now</button>
       </nav>
-          </>
+       {/*Hero*/}
+       <header className="section__container header__container">
+
+{/*title hero*/}
+<div className="header__content">
+  <span className="bg__blur"></span>
+  <span className="bg__blur header__blur"></span>
+
+  <h4>MELHOR ACADEMIA DA CIDADE</h4>
+  <h1><span>MAKE</span> YOUR BODY SHAPE</h1>
+
+  <p>
+    Unleash your potential and embark on a journey towards a stronger,
+    fitter, and more confident you. Sign up for 'Make Your Body Shape' now
+    and witness the incredible transformation your body is capable of!
+  </p>
+
+  {/*button hero*/}
+  <button className="btn">Entre em contato</button>
+</div>
+
+{/*image hero*/}
+<div className="header__image">
+  <img src={header} alt="header" />
+</div>
+
+</header>
+
+    </>
   );
 }
