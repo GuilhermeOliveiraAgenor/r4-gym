e1b6046a896fbe4d5aa6bd8901d5a9a8ab60b4aa
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-02-03 16:33:19 -0300

    feature: add home

---
+++ src/pages/Princing.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+import Pricing from "../components/Pricing";
+
+
+export default function Home() {
+  return (
+    <>
+      <Nav/>
+      <Pricing/>
+      <Footer/>
+    </>
+  );
+}
---
+++ src/pages/SectionImage.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+import Image from "../components/Image";
+
+
+export default function Home() {
+  return (
+    <>
+      <Nav/>
+      <Image/>
+      <Footer/>
+    </>
+  );
+}
