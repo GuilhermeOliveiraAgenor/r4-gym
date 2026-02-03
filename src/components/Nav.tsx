5a0b3752ec58db685b0217509c135bc3fb9ea2c9
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-02-03 17:32:35 -0300

    add routes in nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,14 +7,14 @@ export default function Nav() {
      {/*menu*/}
      <nav>
         <div className="nav__logo">
-          <a href="#"><img src={logo} alt="logo" /></a>
+          <a href="/"><img src={logo} alt="logo" /></a>
         </div>
         <ul className="nav__links">
-          <li className="link"><a href="#">Home</a></li>
-          <li className="link"><a href="#">Program</a></li>
-          <li className="link"><a href="#">Service</a></li>
-          <li className="link"><a href="#">About</a></li>
-          <li className="link"><a href="#">Community</a></li>
+          <li className="link"><a href="/">Home</a></li>
+          <li className="link"><a href="/program">Program</a></li>
+          <li className="link"><a href="/service">Service</a></li>
+          <li className="link"><a href="/about">About</a></li>
+          <li className="link"><a href="/review">Community</a></li>
         </ul>
         <button className="btn">Join Now</button>
       </nav>
