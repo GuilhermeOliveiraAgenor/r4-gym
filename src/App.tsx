3faea319566479f739f7ef2b303674dcf1be04cd
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-02-03 17:21:28 -0300

    feature: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import "./styles/styles.css";
 import Program from "./pages/Program";
 import Home from "./pages/Home";
-
+import Pricing from "./pages/Pricing";
+import Images from "./pages/Image";
+import Review from "./pages/Review";
 
 export default function App() {
   return (
     <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/program" element={<Program/>} />
+        <Route path="/service" element={<Pricing/>}/>
+        <Route path="/about" element={<Images/>}/>
+        <Route path="/review" element={<Review/>}/>
     </Routes>
 );
 }
