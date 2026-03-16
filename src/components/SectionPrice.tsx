205ecc574386da116e996a96a253bd765632e6b3
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-03-16 10:44:58 -0300

    feature: add informations in section price

--- src/components/SectionPrice.tsx
+++ src/components/SectionPrice.tsx
@@ -3,78 +3,145 @@ export default function SectionPrice() {
     <>
       {/*section price cards*/}
       <section className="section__container price__container">
-        <h2 className="section__header">OUR PRICING PLAN</h2>
+        <h2 className="section__header">NOSSOS PLANOS</h2>
         <p className="section__subheader">
-          Our pricing plan comes with various membership tiers, each tailored to cater to different
-          preferences and fitness aspirations.
+        Escolha o plano ideal para o seu objetivo.
+        Temos opções flexíveis com benefícios exclusivos para você evoluir nos treinos.
+        <br></br>
+        <br></br>
+        <div className="pricing">
+        <span className="description_price">A partir de</span> <b>R$ 79,90/mês</b>
+        </div>
         </p>
         <div className="price__grid">
           {/*card*/}
           <div className="price__card">
             <div className="price__card__content">
-              <h4>Basic Plan</h4>
-              <h3>16$</h3>
+              <h4>Plano Mensal</h4>
+              <h3></h3>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                Smart workout plan
+                Musculação
               </p>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                At home workouts
+                Aulas coletivas
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                Sem fidelidade
               </p>
             </div>
-            <button className="btn price__btn">Join Now</button>
+            <button className="btn price__btn">Comece Agora</button>
           </div>
 
           {/*card*/}
           <div className="price__card">
             <div className="price__card__content">
-              <h4>Weekly Plan</h4>
-              <h3>$25</h3>
+              <h4>Plano Trimestral</h4>
+              <h3></h3>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                PRO Gyms
+                Musculação
               </p>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                Smart workout plan
+                Aulas coletivas
               </p>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                At home workouts
+                Avaliação física inicial
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+               Periodização de treino
               </p>
             </div>
-            <button className="btn price__btn">Join Now</button>
+            <button className="btn price__btn">Comece Agora</button>
           </div>
 
           {/*card*/}
           <div className="price__card">
             <div className="price__card__content">
-              <h4>Monthly Plan</h4>
-              <h3>$25</h3>
+              <h4>Plano Anual</h4>
+              <h3></h3>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                ELITE Gyms & Classes
+                Musculação
               </p>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                PRO Gym
+                Aulas coletivas
               </p>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                Smart workout plan
+                4 avaliações físicas durante o ano (1 por trimestre)
               </p>
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                At home workouts
+               Pausa de até 30 dias
               </p>
-
               <p>
                 <i className="ri-checkbox-circle-line"></i>
-                Personal Training
+               Periodização de treino
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                 1 convite por mês - Traga um amigo para treinar com você
               </p>
             </div>
-            <button className="btn price__btn">Join Now</button>
+            <button className="btn price__btn">Comece Agora</button>
+          </div>
+          <div className="price__card">
+            <div className="price__card__content">
+              <h4>Plano Anual com Nutricionista</h4>
+              <h3></h3>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                Musculação
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                Aulas coletivas
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                4 avaliações físicas durante o ano (1 por trimestre)
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+               3 consultas com nutricionista
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                 1 convite por mês - Traga um amigo para treinar com você
+              </p>
+            </div>
+            <button className="btn price__btn">Comece Agora</button>
+          </div>
+          <div className="price__card">
+            <div className="price__card__content">
+              <h4>Horário Especial</h4>
+              <br></br>
+              <h3></h3>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                Acesso restrito (09h às 15h)
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                Musculação
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                Aulas coletivas
+              </p>
+              <p>
+                <i className="ri-checkbox-circle-line"></i>
+                Sem fidelidade
+              </p>
+            </div>
+            <button className="btn price__btn">Comece Agora</button>
           </div>
         </div>
       </section>
